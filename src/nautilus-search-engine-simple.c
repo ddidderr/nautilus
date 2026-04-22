ddidderr/nautilus

commit 6b771727c5e42714c24784c825904b214cd97be9

search-engine-simple: Simplify visited tracking snippet

src/nautilus-search-engine-simple.c
@@ -424,22 +424,11 @@ visit_directory (GFile            *dir,
         if (recursive)
         {
             const char *id = g_file_info_get_attribute_string (info, G_FILE_ATTRIBUTE_ID_FILE);
-            gboolean visited = FALSE;
-            if (id != NULL)
-            {
-                if (g_hash_table_lookup_extended (data->visited,
-                                                  id, NULL, NULL))
-                {
-                    visited = TRUE;
-                }
-                else
-                {
-                    g_hash_table_insert (data->visited, g_strdup (id), NULL);
-                }
-            }
 
-            if (!visited)
+            if (id != NULL &&
+                !g_hash_table_contains (data->visited, id))
             {
+                g_hash_table_add (data->visited, g_strdup (id));
                 g_queue_push_tail (data->directories, g_steal_pointer (&child));
             }
         }

commit b125c7d7e7466e4a710f63f033dc3f9d6e4cd749

search-engine-simple: Fix recursion for directories with no ID
Fixes recursive search in SMB shares.
Regression from 6b771727c5e42714c24784c825904b214cd97be9

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/4200

src/nautilus-search-engine-simple.c
@@ -214,8 +214,11 @@ visit_directory (NautilusSearchEngineSimple *self,
         {
             const char *id = g_file_info_get_attribute_string (info, G_FILE_ATTRIBUTE_ID_FILE);
 
-            if (id != NULL &&
-                !g_hash_table_contains (self->visited, id))
+            if (id == NULL)
+            {
+                g_queue_push_tail (self->directories, g_steal_pointer (&child));
+            }
+            else if (!g_hash_table_contains (self->visited, id))
             {
                 g_hash_table_add (self->visited, g_strdup (id));
                 g_queue_push_tail (self->directories, g_steal_pointer (&child));